93d25769ba2547b71e8fdc86e2c83bf4ff6a761e
DEVASHISH-PRASAD <devashishprasadbca2022@gmail.com>
2024-03-11 11:34:03 +0530

    Added css to pokedex

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Pokedex from './components/pokedex/pokedex'
-import Search from './components/Search/search'
+import "./App.css";
+import Pokedex from "./components/pokedex/pokedex";
 
 function App() {
- 
   return (
     <>
-      <Pokedex/>
+      <Pokedex />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
